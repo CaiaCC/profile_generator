fe614268b7c24d219adfd4b277a238b7a6cd8c8d
Caia Chuang <caiachuang@gmail.com>
2020-12-21 17:17:10 -0800

    Clean code.

--- survey.js
+++ survey.js
@@ -5,20 +5,20 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What\'s your name? Nicknames are also acceptable :\)', (name) => {
-	rl.question('What\'s an activity you like doing? ', (activity) => {
-		rl.question('What do you listen to while doing that? ', (music) => {
-		  rl.question('Which meal is your favourite meal of a day \(eg: dinner, brunch, etc.\)', (favouriteMeal) => {
-			  rl.question('What\'s your favourite thing to eat for that meal?', (favouriteDish) => {
-				  rl.question('Which sport is your absolute favourite?', (sport) => {
-						rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (superpower) => {
-							console.log(`${name} loves listening to ${music} while ${activity}, devouring ${favouriteDish} for ${favouriteMeal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`);
-							
-							rl.close();
-						});
-					});
-				});	
-			});		
-		});
-	});
+rl.question('What\'s your name? Nicknames are also acceptable :\ ', (name) => {
+    rl.question('What\'s an activity you like doing? ', (activity) => {
+        rl.question('What do you listen to while doing that? ', (music) => {
+          rl.question('Which meal is your favourite meal of a day \(eg: dinner, brunch, etc.\) ', (favouriteMeal) => {
+              rl.question('What\'s your favourite thing to eat for that meal? ', (favouriteDish) => {
+                  rl.question('Which sport is your absolute favourite? ', (sport) => {
+                    rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
+                      console.log(`${name} loves listening to ${music} while ${activity}, devouring ${favouriteDish} for ${favouriteMeal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`);
+                        
+                      rl.close();
+                    });
+                    });
+                });	
+            });		
+        });
+    });
 });
